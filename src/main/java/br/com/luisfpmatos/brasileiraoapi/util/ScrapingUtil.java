9cd1bf4e1eb758358b4437678a413809b694ae6d
Luis Matos <luismatos000@gmail.com>
2022-09-21 22:22:41 -0300

    obtendo status da partida

--- src/main/java/br/com/luisfpmatos/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/luisfpmatos/brasileiraoapi/util/ScrapingUtil.java
@@ -32,11 +32,37 @@ public class ScrapingUtil {
 			String title = document.title();
 			LOGGER.info("Titulo da pagina: {}", title);
 
+			StatusPartida statusPartida = obtemStatusPartida(document);
 		} catch (IOException e) {
 			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
 		}
 
 		return partida;
+	}
+
+	public StatusPartida obtemStatusPartida(Document document) {
+		StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		if (!isTempoPartida) {
+			String tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			statusPartida = StatusPartida.PARTIDA_EM_ANDAMENTO;
+
+			if (tempoPartida.contains("Pênaltis")) {
+				statusPartida = StatusPartida.PARTIDA_PENALTIS;
+			}
+
+			LOGGER.info(tempoPartida);
+		}
+
+		isTempoPartida = document.select("div[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+
+		if (!isTempoPartida) {
+			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+		}
+		
+		LOGGER.info(statusPartida.toString());
+		return statusPartida;
 	}
 
 }
