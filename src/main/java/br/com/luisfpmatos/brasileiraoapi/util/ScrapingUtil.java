39dd508af87dad4e7859ec171ac5d38400af1c7b
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2022-09-22 19:56:02 -0300

    recuperando nome/logo

--- src/main/java/br/com/luisfpmatos/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/luisfpmatos/brasileiraoapi/util/ScrapingUtil.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +17,7 @@ public class ScrapingUtil {
 	private static final String COMPLEMENTO_URL = "&hl=pt-BR";
 
 	public static void main(String[] args) {
-		String url = BASE_URL_GOOGLE + "cruzeiro+x+vasco+21/09/2022" + COMPLEMENTO_URL;
+		String url = BASE_URL_GOOGLE + "Rionegro+Águilas+x+Union+Magdalena+22/09/2022" + COMPLEMENTO_URL;
 		ScrapingUtil scraping = new ScrapingUtil();
 		scraping.obtemInformacoesPartida(url);
 	}
@@ -34,8 +35,24 @@ public class ScrapingUtil {
 
 			StatusPartida statusPartida = obtemStatusPartida(document);
 			LOGGER.info("Status partida: {}", statusPartida);
-			String tempoPartida = obtemTempoPartida(document);
-			LOGGER.info("Tempo partida: {}", tempoPartida);
+
+			if (statusPartida != StatusPartida.PARTIDA_NAO_INICIADA) {
+				String tempoPartida = obtemTempoPartida(document);
+				LOGGER.info("Tempo partida: {}", tempoPartida);
+			}
+
+			String nomeEquipeCasa = recuperarNomeEquipeCasa(document);
+			LOGGER.info("Nome Equipe Casa: {}", nomeEquipeCasa);
+
+			String nomeEquipeVisitante = recuperarNomeEquipeVisitante(document);
+			LOGGER.info("Nome Equipe Visitante: {}", nomeEquipeVisitante);
+
+			String urlLogoEquipeCasa = recuperarLogoEquipeCasa(document);
+			LOGGER.info("Url Logo Equipe Casa: {}", urlLogoEquipeCasa);
+
+			String urlLogoEquipeVisitante = recuperarLogoEquipeVisitante(document);
+			LOGGER.info("Url Logo Equipe Visitante: {}", urlLogoEquipeVisitante);
+			
 		} catch (IOException e) {
 			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
 		}
@@ -55,7 +72,6 @@ public class ScrapingUtil {
 				statusPartida = StatusPartida.PARTIDA_PENALTIS;
 			}
 
-			LOGGER.info(tempoPartida);
 		}
 
 		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
@@ -82,11 +98,9 @@ public class ScrapingUtil {
 		if (!isTempoPartida) {
 			tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first()
 					.text();
-
 		}
 
 		return corrigTempoPartida(tempoPartida);
-
 	}
 
 	public String corrigTempoPartida(String tempo) {
@@ -101,4 +115,23 @@ public class ScrapingUtil {
 		return tempoPartida;
 	}
 
+	public String recuperarNomeEquipeCasa(Document document) {
+		Element element = document.selectFirst("div[class=imso_mh__first-tn-ed imso_mh__tnal-cont imso-tnol]");
+		return element.select("span").text();
+	}
+
+	public String recuperarNomeEquipeVisitante(Document document) {
+		Element element = document.selectFirst("div[class=imso_mh__second-tn-ed imso_mh__tnal-cont imso-tnol]");
+		return element.select("span").text();
+	}
+
+	public String recuperarLogoEquipeCasa(Document document) {
+		Element element = document.selectFirst("div[class=imso_mh__first-tn-ed imso_mh__tnal-cont imso-tnol]");
+		return "https:" + element.select("img[class=imso_btl__mh-logo]").attr("src");
+	}
+	
+	public String recuperarLogoEquipeVisitante(Document document) {
+		Element element = document.selectFirst("div[class=imso_mh__second-tn-ed imso_mh__tnal-cont imso-tnol]");
+		return "https:" + element.select("img[class=imso_btl__tm-lo-luf]").attr("src");
+	}
 }
